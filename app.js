3a6185e79639e9290282c32e576292ea7d1d250d
johnny <johnny@debian>
2015-05-29 22:32:55 +0800

    test

--- app.js
+++ app.js
@@ -65,7 +65,7 @@ app.post('/wechat', handleweixin.handle);
 app.post('/studentsubscribe', handleweixin.StuSubscribe);
 app.post('/testuploadworklog', handleweixin.testuploadworklog);
 app.get('/weixinquery/:id', handleweixin.WeixinQuery);
-app.get('/wxreg/:id', handleweixin.WeixinRegister);
+//app.get('/wxreg/:id', handleweixin.WeixinRegister);
 
 //app.get('/weixinquerytext/:id', handleweixin.weixinquerytext);
 //app.get('/weixinqueryimage/:id', handleweixin.weixinqueryimage);
