6a71dd2e7076e06fde4b77c21c3bad0589a6a96b
johnny <johnny@debian>
2015-06-04 21:47:33 +0800

    excel none

--- model/exportlogs.js
+++ model/exportlogs.js
@@ -376,10 +376,10 @@ var SaveToFile = function(file_name, sheet_name, data, callback) {
 			    "项目名称": data[i][3],
 			    "教师姓名": data[i][4],
 			    "序号": data[i][5],
-			    "提交地点": {value:data[i][6], hyperlink:'./'+data[i][6]},
+			    "提交地点": data[i][6],//{value:data[i][6], hyperlink:'./'+data[i][6]},
 			    "时间": data[i][7],
 			    "文本日志": data[i][8],
-			    "图片日志": {value:data[i][9], hyperlink:'./'+data[i][9]}
+			    "图片日志": data[i][9]//{value:data[i][9], hyperlink:'./'+data[i][9]}
 			});
 			
 			// writer.addRow({
