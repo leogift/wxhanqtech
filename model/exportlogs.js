f0ba733fc3c466e3fa538be543db86ca75416bdb
johnny <johnny@debian>
2015-06-04 21:10:45 +0800

    excel if link is none, will be error

--- model/exportlogs.js
+++ model/exportlogs.js
@@ -329,23 +329,28 @@ var SaveToFile = function(file_name, sheet_name, data, callback) {
 
 		for(var i=1; i<data.length; i++)
 		{
-			console.log('logText:' + data[i][8]);
+			//console.log('logText:' + data[i][8]);
 			console.log('logLocation:' + data[i][6]);
 			console.log('logPic:' + data[i][9]);
 
 			var logLocation = data[i][6];
 			var logPic = data[i][9];
+			var locationRaw = {value:data[i][6], hyperlink:'./'+data[i][6]};
+			var picRaw = {value:data[i][9], hyperlink:'./'+data[i][9]};
 
 			if(logLocation=="")
 			{
 				console.log('logLocation not_found');
 				logLocation = 'not_found';
+				locationRaw = 'not_found';
 			}
+			
 
 			if(logPic=="")
 			{
 				console.log('logPic not_found');
 				logPic = 'not_found';
+				picRaw = 'not_found';
 			}
 				
 
@@ -358,10 +363,12 @@ var SaveToFile = function(file_name, sheet_name, data, callback) {
 			    "教师姓名": data[i][4],
 			    "序号": data[i][5],
 			    //交地点": {value:data[i][6], hyperlink:'./'+data[i][6]},
-			    "提交地点": {value:logLocation, hyperlink:logLocation},
+			    //"提交地点": {value:logLocation, hyperlink:logLocation},
+			    "提交地点": locationRaw,
 			    "时间": data[i][7],
 			    "文本日志": '' + data[i][8],
-			    "图片日志": {value:logPic, hyperlink:logPic},
+			    //"图片日志": {value:logPic, hyperlink:logPic},
+			    "图片日志": picRaw,
 			});
 			// writer.addRow({
 			//     index: data[i][0],
