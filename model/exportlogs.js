441f089035b3edebc66f39d85de42bd460e4a536
johnny <johnny@debian>
2015-06-11 22:49:06 +0800

    test chinese file name download failed!

--- model/exportlogs.js
+++ model/exportlogs.js
@@ -561,20 +561,20 @@ exports.SysWorklogExport = function (req, res, role) {
 								if(exists)
 								{
 									console.log('start to download!');
-									//res.download(zipFile);
-
-									var filename = zipFile; 
-									var userAgent = (req.headers['user-agent']||'').toLowerCase();
-									 
-									if(userAgent.indexOf('msie') >= 0 || userAgent.indexOf('chrome') >= 0) {
-									    res.setHeader('Content-Disposition', 'attachment; filename=' + encodeURIComponent(filename));
-									} else if(userAgent.indexOf('firefox') >= 0) {
-									    res.setHeader('Content-Disposition', 'attachment; filename*="utf8\'\'' + encodeURIComponent(filename)+'"');
-									} else {
-									    /* safari等其他非主流浏览器只能自求多福了 */
-									    res.setHeader('Content-Disposition', 'attachment; filename=' + new Buffer(filename).toString('binary'));
-									}
 									res.download(zipFile);
+
+									// var filename = zipFile; 
+									// var userAgent = (req.headers['user-agent']||'').toLowerCase();
+									 
+									// if(userAgent.indexOf('msie') >= 0 || userAgent.indexOf('chrome') >= 0) {
+									//     res.setHeader('Content-Disposition', 'attachment; filename=' + encodeURIComponent(filename));
+									// } else if(userAgent.indexOf('firefox') >= 0) {
+									//     res.setHeader('Content-Disposition', 'attachment; filename*="utf8\'\'' + encodeURIComponent(filename)+'"');
+									// } else {
+									//     /* safari等其他非主流浏览器只能自求多福了 */
+									//     res.setHeader('Content-Disposition', 'attachment; filename=' + new Buffer(filename).toString('binary'));
+									// }
+									// res.download(zipFile);
 								}
 								else
 								{
