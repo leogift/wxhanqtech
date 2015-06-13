ad0e16ed565d8830fd6158c060adfc6fd4babc1f
johnny <johnny@debian>
2015-06-13 19:45:13 +0800

    process.on

--- app.js
+++ app.js
@@ -267,22 +267,27 @@ RunAutoArchiveSchedule();
 
 
 
-var domain = require('domain');
+// var domain = require('domain');
 
-//引入一个domain的中间件，将每一个请求都包裹在一个独立的domain中
-//domain来处理异常
-app.use(function (req,res, next) {
-  var d = domain.create();
-  //监听domain的错误事件
-  d.on('error', function (err) {
-    console.log(err);
-    res.statusCode = 500;
-    res.json({sucess:false, messag: '服务器异常'});
-    d.dispose();
-  });
+// //引入一个domain的中间件，将每一个请求都包裹在一个独立的domain中
+// //domain来处理异常
+// app.use(function (req,res, next) {
+//   var d = domain.create();
+//   //监听domain的错误事件
+//   d.on('error', function (err) {
+//     console.log(err);
+//     res.statusCode = 500;
+//     res.json({sucess:false, messag: '服务器异常'});
+//     d.dispose();
+//   });
   
-  d.add(req);
-  d.add(res);
-  d.run(next);
+//   d.add(req);
+//   d.add(res);
+//   d.run(next);
+// });
+
+
+process.on('uncaughtException', function (err) {
+  logger.error(err);
 });
 
