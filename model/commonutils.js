6ff92d8967d350a39442f57743bf70cc4a2908c8
johnny <johnny@debian>
2015-06-24 10:31:17 +0800

    dis

--- model/commonutils.js
+++ model/commonutils.js
@@ -1045,6 +1045,11 @@ exports.CalcLatLonDistance = function (lat1,lng1,lat2,lng2) {
 
 	console.log('CalcLatLonDistance lat1=' + lat1 + ' lng1=' + lng1 + ' lat2=' + lat2 + ' lng2=' + lng2);
 
+	lat1 += 0;
+	lng1 += 0;
+	lat2 += 0;
+	lng2 += 0;
+
 	//otherwise return NaN
 	if(lat1==lat2 && lng1==lng2)
 		return 0;
